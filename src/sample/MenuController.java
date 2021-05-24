e9bcc74d3e7b8879824c9dc57bcee628b9eb01f7
marina-micheal <marinaroshdy@gmail.com>
2021-05-24 11:30:13 +0200

    edited

--- src/sample/MenuController.java
+++ src/sample/MenuController.java
@@ -1,47 +1,20 @@
 package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Button;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
-import java.net.URL;
+
+
 
 public class MenuController {
-    public void showUserOne(javafx.event.ActionEvent actionEvent) throws IOException {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("user1.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
 
-    public void showAccount(javafx.event.ActionEvent actionEvent) throws IOException {
+    @FXML
+    private AnchorPane rootPane;
+
+    public void showUser1(javafx.event.ActionEvent actionEvent) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("account.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("user1.fxml"));
+            rootPane.getChildren().setAll(pane);
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -49,65 +22,26 @@ public class MenuController {
 
     public void showHelp(javafx.event.ActionEvent actionEvent) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("help.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("help.fxml"));
+            rootPane.getChildren().setAll(pane);
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-   public void ExitGame(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void showAccount(javafx.event.ActionEvent actionEvent) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("account.fxml"));
+            rootPane.getChildren().setAll(pane);
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
 
-
-
-
-    public void showMenu(javafx.event.ActionEvent actionEvent) throws IOException {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public void exit() {
+        System.exit(0);
     }
 
-    public void showBoard(javafx.event.ActionEvent actionEvent) throws IOException {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.NONE);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ABC");
-            stage.setScene(new Scene(root1));
-            stage.show();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+
 }
